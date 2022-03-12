76bd329238e8ffb8ceaafe79895db308073a8f3a
Tommy Bolger <tommy.bolger@gmail.com>
2022-03-12 01:24:40 -0800

    Fixed demo script.

--- demo/test.php
+++ demo/test.php
@@ -1,14 +1,14 @@
-#!/usr/bin/env php
 <?php
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
 use GuzzleHttp\Client;
-use Steam\Configuration;
-use Steam\Runner\GuzzleRunner;
-use Steam\Runner\DecodeJsonStringRunner;
-use Steam\Steam;
-use Steam\Utility\GuzzleUrlBuilder;
+use SquegTech\Steam\Command\Apps\GetAppList;
+use SquegTech\Steam\Configuration;
+use SquegTech\Steam\Runner\GuzzleRunner;
+use SquegTech\Steam\Runner\DecodeJsonStringRunner;
+use SquegTech\Steam\Steam;
+use SquegTech\Steam\Utility\GuzzleUrlBuilder;
 
 $steam = new Steam(new Configuration([
     Configuration::STEAM_KEY => '<insert steam key here>'
@@ -17,6 +17,6 @@ $steam->addRunner(new GuzzleRunner(new Client(), new GuzzleUrlBuilder()));
 $steam->addRunner(new DecodeJsonStringRunner());
 
 /** @var array $result */
-$result = $steam->run(new \Steam\Command\Apps\GetAppList());
+$result = $steam->run(new GetAppList());
 
 var_dump($result);
